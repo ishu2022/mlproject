099e07aeb2fd6ec0f566622030f57f6f34bec229
ishu2022 <mohodishika2022@gmail.com>
2026-03-19 16:16:29 +0530

    Add data ingestion and data transformation

--- setup.py
+++ setup.py
@@ -4,24 +4,18 @@ from typing import List
 HYPEN_E_DOT = '-e .'
 
 def get_requirements(file_path: str) -> List[str]:
+    requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [
-            req.strip() for req in requirements
-            if req.strip() and req.strip() != HYPEN_E_DOT and not req.startswith("#")
-        ]
+        requirements = [req.replace("\n", "") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
     return requirements
 
 setup(
     name='mlproject',
     version='0.0.1',
     author='Ishika',
-    author_email='mohodishika2022@gmail.com',
-    description="End-to-End Machine Learning Project",
-    
-    package_dir={"": "src"},               
-    packages=find_packages(where="src"),  
-    
-    install_requires=get_requirements('requirements.txt'),
-    python_requires=">=3.8",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
