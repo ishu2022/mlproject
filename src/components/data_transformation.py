488cebc60bdbde1867881c4cb68f8518f096bbb0
ishu2022 <mohodishika2022@gmail.com>
2026-04-01 19:01:54 +0530

    updated home.html with new UI

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -121,4 +121,7 @@ class DataTransformation:
                 self.data_transformation_config.preprocessor_obj_file_path,
             )
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e,sys)                                                  
+
+        
+                  
